5dda5fd974f13aeefd973cef923022ce030283d0
Franco Espinoza <francoespinoza1107@gmail.com>
2026-02-26 17:50:02 -0300

    fix cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,20 +1,27 @@
 import cors from 'cors';
 
+const FRONTEND_URLS = process.env.FRONTEND_URL
+    ? process.env.FRONTEND_URL.split(',')
+    : [];
+
 const ACCEPTED_ORIGINS = [
-    process.env.FRONTEND_URL || 'http://localhost:5173'
-]
+    'http://localhost:5173',
+    ...FRONTEND_URLS
+];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
     origin: (origin, callback) => {
-        if (acceptedOrigins.includes(origin as string)) {
+
+        if (!origin) {
             return callback(null, true);
         }
 
-        if (!origin) {
-            return callback(null, true)
+        if (!acceptedOrigins.includes(origin)) {
+            console.error(`🔴 CORS Bloqueado. Origen recibido: '${origin}'. Origenes permitidos:`, acceptedOrigins);
+            return callback(new Error('Not allowed by CORS'));
         }
 
-        return callback(new Error('Not allowed by CORS'))
+        return callback(null, true);
     },
     credentials: true
-})
+});
